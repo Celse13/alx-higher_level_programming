616905bd5e1fd128b0e80962b6813ead32d116d5
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-08-11 17:47:22 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 from add_0 import add
 
 if __name__ == "__main__":
-        
-        a = 1
-            b = 2
-                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-                    
+    """provide ouput of 1 and 2
+    """
+    
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    
