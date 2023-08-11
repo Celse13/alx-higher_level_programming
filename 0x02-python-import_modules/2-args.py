6e485737464cc4cc805f9f76156573d3f954e037
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-08-11 22:28:32 +0200

    Updated task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
 
 if __name__ == "__main__":
+    from sys import argv
     word = "argument"
     arguments_from_pipe = argv[1:]
     length = len(arguments_from_pipe)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
+    
+    arguments_from_pipe = argv[1:]
+    length = len(arguments_from_pipe)
 
-arguments_from_pipe = argv[1:]
-length = len(arguments_from_pipe)
+    summation = 0
 
-summation = 0
-
-for x in range(length):
-    summation += int(arguments_from_pipe[x])
-        
-print(f"{summation}")
+    for x in range(length):
+        summation += int(arguments_from_pipe[x])
+            
+    print(f"{summation}")
