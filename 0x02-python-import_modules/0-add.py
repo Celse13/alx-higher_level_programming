4364371045b4b654a9501fed9c3f6bb146802e77
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-08-11 19:30:48 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
+    """provide ouput of 1 and 2
+    """
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    
