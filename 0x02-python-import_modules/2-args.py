14a600f27a8a32e64828ef223606389117da22e6
Celse13 <rugiracelse13@gmail.com>
2023-08-12 08:19:45 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,16 @@ if __name__ == "__main__":
     word = "argument"
     arguments_from_pipe = argv[1:]
     length = len(arguments_from_pipe)
+
     if length == 0:
-        word += "s"
-        print(f"{length} {word}")
+        word += "s."
+        print("{} {}".format(length, word))
     elif length == 1:
         word += ":"
-        print(f"{length} {word}")
+        print("{} {}".format(length, word))
     else:
         word += "s:"
-        print(f"{length} {word}")
+        print("{} {}".format(length, word))
+
     for i in range(length):
-        print(f"{i + 1}: {arguments_from_pipe[i]}")
+        print("{}: {}".format(i + 1, arguments_from_pipe[i]))
