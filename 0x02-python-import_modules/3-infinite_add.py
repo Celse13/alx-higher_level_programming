7893852b8982f87a4fa149cc99ee582d5020ea48
Celse13 <rugiracelse13@gmail.com>
2023-08-12 08:24:23 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    from sys import argv
-    
     arguments_from_pipe = argv[1:]
     length = len(arguments_from_pipe)
-
     summation = 0
-
     for x in range(length):
         summation += int(arguments_from_pipe[x])
-            
-    print(f"{summation}")
+    print("{}".format(summation))
