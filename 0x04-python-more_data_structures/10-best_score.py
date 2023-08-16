5b550aa91a10282790a30cd9889a3bfb5c04e05b
Celse13 <rugiracelse13@gmail.com>
2023-08-16 11:00:04 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_value = list(a_dictionary.keys())[0]
+        for key, value in a_dictionary.items():
+            if value > a_dictionary[max_value]:
+                max_value = key
+        return max_value
