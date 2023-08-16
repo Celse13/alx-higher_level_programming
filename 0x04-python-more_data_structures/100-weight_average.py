758e7f70cc1fdbbb48654b4797c349d182b7c538
Celse13 <rugiracelse13@gmail.com>
2023-08-16 12:27:56 -0700

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    up_fraction = 0
+    down_fraction = 0
+    if my_list:
+        for element in my_list:
+            up_fraction += element[0] * element[1]
+            down_fraction += element[1]
+            operation = up_fraction / down_fraction
+            return (operation)
+    else:
+        return 0
