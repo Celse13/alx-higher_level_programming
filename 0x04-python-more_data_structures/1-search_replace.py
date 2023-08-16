ee1a28757d62758bcf013e79f5d2ba3c865f780c
Celse13 <rugiracelse13@gmail.com>
2023-08-16 07:56:08 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    modified_list = list()
+    modified_list = my_list[:]
+    for element in modified_list:
+        if element == search:
+            element = replace
+    return modified_list
