d3d53291f5ddd17aabbbb4e29d9fab62b9625a95
Celse13 <rugiracelse13@gmail.com>
2023-08-28 08:06:26 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     count_element = 0
     try:
-        for index in range(x):
-            print(my_list[index], end='')
+        for val in range(x):
+            print(my_list[val], end='')
             count_element += 1
     except IndexError:
         None
