9d626ba227c5a227f499c7900d34be3b89176765
celse13 <rugiracelse13@gmail.com>
2023-08-29 14:38:11 +0000

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Definition of a Square class."""
+
+
+class Square:
+    """Representation of a square."""
+
+    def __init__(self, size=0):
+        """Constructor for the square class.
+        Args:
+            size (int): size private attribute.
+        """
+    @property
+    def size(self):
+        """Retriving the value of size."""
+        return (self.size)
+
+    @size.setter
+    def size(self, value):
+        """Setting the value of size.
+        Args:
+        value (int): setting value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
+
