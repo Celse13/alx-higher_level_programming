dd111e9627124ac7194dc9649e84649f46166b1c
celse13 <rugiracelse13@gmail.com>
2023-08-29 13:47:25 +0000

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Definition of  a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """Representation of a square."""
     pass
