15c9a22c27b2012899fc88eac7cd84db43735956
celse13 <rugiracelse13@gmail.com>
2023-08-29 13:56:07 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of a Square class."""
+
+class Square:
+    """Representation of a square."""
+    
+    def __init__(self, size):
+        """Constructor for the square class.
+        Args:
+            size (int): size private attribute
+        """
+        self.size = size
