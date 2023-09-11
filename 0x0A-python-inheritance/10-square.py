0a7469fb8a720cc1ed00ba831ef10f378cd30588
Celse13 <rugiracelse13@gmail.com>
2023-09-11 12:21:46 -0700

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Dynamic importation of Rectangle Class."""
-
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Representation of Square."""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
