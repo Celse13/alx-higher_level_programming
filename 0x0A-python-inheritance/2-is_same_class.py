986408abe6ee3a7bcda566f383be03779f668eb7
Celse13 <rugiracelse13@gmail.com>
2023-09-11 10:45:51 -0700

    2-is_same_class.py && git push

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,8 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-    """Checking if obj is an instance of a_class."""
-    if isinstance(obj, a_class):
-        return (True)
-    else:
-        return (False)
+        """Checking if obj is an instance of a_class."""
+            return isinstance(obj, a_class)
