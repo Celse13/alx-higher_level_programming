c3639600976153410e2b80ea909a86f03fcc1d60
Celse13 <rugiracelse13@gmail.com>
2023-09-11 11:12:47 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Checking whether an object is an instance of a class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checking if obj is an instance of a_class.
+    Args:
+        obj: an object of class
+        a_class: a class
+    Returns:
+        True if obj is a instance of a_class
+    """
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return (True)
+    return (False)
