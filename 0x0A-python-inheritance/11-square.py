b472e3ca1fc3f91a8e60dc97481c7941963f355d
Celse13 <rugiracelse13@gmail.com>
2023-09-11 12:37:27 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Dynamic importation of Rectangle Class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representation of Square."""
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return("[Square] " + str(self.__size) + "/" + str(self.__size))
