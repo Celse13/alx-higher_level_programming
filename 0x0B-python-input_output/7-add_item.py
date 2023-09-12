2dd36e38fb56190a756c2aa7ec6cacf893e0babe
Celse13 <rugiracelse13@gmail.com>
2023-09-12 12:34:55 -0700

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Adds command-line arguments to a Python list,
+saves it to 'add_item.json',
+and loads it back..
+"""
+from sys import argv
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(argv[1:])
+    save_to_json_file(items, "add_item.json")
