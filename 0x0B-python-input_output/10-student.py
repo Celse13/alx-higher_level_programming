d2c977f6c3c41bef316dd85fd5abef0d46f53980
Celse13 <rugiracelse13@gmail.com>
2023-09-12 13:01:26 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """Representation of the student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor for the student."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary description with simple data."""
+
+        if (type(attrs) == list and
+                all(type(val) == str for val in attrs)):
+            return {u: getattr(self, u) for u in attrs if hasattr(self, u)}
+        return (self.__dict__)
