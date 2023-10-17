205e47a0812f8bb8ab3aa1decac717fdf16596b9
Celse13 <rugiracelse13@gmail.com>
2023-10-17 12:20:20 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,8 @@
 -- Creating a new table and initialize it with fields such as id and name
-CREATE TABLE IF NOT EXISTS `first_table` (
+CREATE TABLE IF NOT EXISTS `second_table` (
 	`id` INT,
 	`name` VARCHAR(256),
-	`score` INT
-);
+	`score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
