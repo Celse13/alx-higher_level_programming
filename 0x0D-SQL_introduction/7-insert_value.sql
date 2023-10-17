9ed8912b49162f076456bbc134dc1022359045c8
Celse13 <rugiracelse13@gmail.com>
2023-10-17 11:59:12 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --- Inserting a new row with field in first_table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
