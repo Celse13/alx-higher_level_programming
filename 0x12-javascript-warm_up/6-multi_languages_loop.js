c7a057a807c1f5ffdce3bc54bca8fff39201aa04
Celse13 <rugiracelse13@gmail.com>
2023-11-13 10:18:19 -0800

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const programming_language = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < programming_language.length; i++) {
+    console.log(programming_language[i]);
+}
