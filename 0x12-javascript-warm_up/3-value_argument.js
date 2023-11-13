ab0bed7280edf3ed4fb52006228223775ba8411a
Celse13 <rugiracelse13@gmail.com>
2023-11-13 09:55:59 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length > 2) {
-	const argument_length = process.argv.length - 2;
-
-	for (let i = 0; i < argument_length; i++) {
-		console.log(process.argv[2 + i]);
-	}
+if (process.argv[2] !== undefined) {
+	console.log(process.argv[2]);
+} else {
+	console.log('No argument');
 }
