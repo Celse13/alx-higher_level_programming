c1037b1b94aafb3b474a9e9ee647d7e1f8cc7d25
Celse13 <rugiracelse13@gmail.com>
2023-11-13 12:05:46 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
-function factorial(num) {
-  if (num === 0 || num === 1 || isNaN(num)) {
-		return 1;
-	} if (num < 0) {
-		return -1;
-	} else return num * (factorial(num - 1));
+const process = require('process');
+
+function factorial(a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  if (a < 0) {
+    return -1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 }
 
 console.log(factorial(parseInt(process.argv[2])));
