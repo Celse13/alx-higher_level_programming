86198c2b32ad1ba1d73bfb1604e005a1fbf9a96c
Celse13 <rugiracelse13@gmail.com>
2023-11-13 11:56:28 -0800

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function factorial(num) {
+  if (num === 0 || num === 1) {
+		return 1;
+	} else return num * (factorial(num - 1));
+}
+
+console.log(factorial(parseInt(process.argv[2])));
