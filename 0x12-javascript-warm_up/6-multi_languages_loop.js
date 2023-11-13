5a827f2300306a190f7cd5172d727ce9d4b9e103
Celse13 <rugiracelse13@gmail.com>
2023-11-13 10:33:58 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node
-let programming_lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-for (i = 0; i < 3; i++) {
-	console.log(programming_lang[i]);
+!/usr/bin/node
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
 }
