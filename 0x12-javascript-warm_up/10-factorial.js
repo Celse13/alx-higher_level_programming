ffbc50a88edb9833b9a9e184fafdeb7919267504
Celse13 <rugiracelse13@gmail.com>
2023-11-13 11:59:49 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function factorial(num) {
-  if (num === 0 || num === 1) {
+  if (num === 0 || num === 1 || isNaN(num)) {
 		return 1;
 	} else return num * (factorial(num - 1));
 }
