eb2b29425b278eded7d6b3ce1a6452e083c4d561
Celse13 <rugiracelse13@gmail.com>
2023-11-14 12:26:56 -0800

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const SquareFromFive = require('./5-square');
+
+class Square extends SquareFromFive {
+  charPrint (c) {
+    if (c === undefined) c = 'X';
+    let i = 0;
+    while (i < this.height) {
+      console.log(c.repeat(this.width));
+      i++;
+    }
+  }
+}
+
+module.exports = Square
