1470790ba9e474a5e5bd62c9330ad22aa879e93c
Celse13 <rugiracelse13@gmail.com>
2023-11-14 13:45:22 -0800

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const newReverse = [];
-  for (let i = list.length; i >= 0; i--) {
-    newReverse.push(list[i]);
+  for (const ele of list) {
+    newReverse.unshift(ele);
   }
   return newReverse;
 };
