7d1acc13f97a3e27ec1c0a401e8ed9e751f14493
Celse13 <rugiracelse13@gmail.com>
2023-11-19 12:49:32 -0800

    13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Retrieving the states from the database using ORM."""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=db_engine)
+    Base.metadata.create_all(db_engine)
+    session = Session()
+
+    state_query = session.query(State).filter(State.name.like(f'%a%')).all()
+    if state_query:
+        for state in state_query:
+            session.delete(state)
+        session.commit()
+
+    session.close()
