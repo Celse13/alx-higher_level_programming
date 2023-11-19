b9195b474e73dcbd2b8b44f068ad631337575200
Celse13 <rugiracelse13@gmail.com>
2023-11-19 08:01:46 -0800

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Retrieving the states from the database using ORM."""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    data_base = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3], port=3306
+    )
+    database_cursor = data_base.cursor()
+
+    query = "SELECT name FROM cities WHERE state_id = \
+        (SELECT id FROM states WHERE name LIKE BINARY %s) ORDER BY cities.id;"
+    args = (sys.argv[4] + '%',)
+
+    database_cursor.execute(query, args)
+
+    data_base_rows = database_cursor.fetchall()
+
+    for item in data_base_rows:
+        print(item)
+
+    database_cursor.close()
+    data_base.close()
