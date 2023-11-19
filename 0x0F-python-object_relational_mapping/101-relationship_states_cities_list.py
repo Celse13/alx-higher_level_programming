f6e6d1efb8605396356f2f8440c14d7f7ffa16e4
Celse13 <rugiracelse13@gmail.com>
2023-11-19 14:50:32 -0800

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""model state
-"""
+"""Retrieving the states from the database using ORM."""
+
+
 from sqlalchemy import create_engine
+from sys import argv
 from relationship_city import City
 from relationship_state import State, Base
 from sqlalchemy.orm import sessionmaker
-import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=db_engine)
     session = Session()
-    objs = session.query(City).outerjoin(State).all()
-    if objs:
-        for city in objs:
-            print(f"{city.id}: {city.name} -> {city.state.name}")
+    Base.metadata.create_all(db_engine)
+
+    city_query = session.query(State).order_by(State.id)
+    for state in city_query:
+        print("{state.id}: {state.name}")
+        for city in state.cities:
+            print("    {city.id}: {city.name}")
