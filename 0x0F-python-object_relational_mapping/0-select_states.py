25efe391692e772431b1278cdc588be8324a2437
Celse13 <rugiracelse13@gmail.com>
2023-11-19 05:03:06 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Retrieving the states from the database using ORM."""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-
-    data_base = MySQLdb.connect(
-            host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306
-            )
-
-    database_cursor = data_base.cursor()
-    database_cursor.execute("SELECT * FROM states")
-
-    data_base_rows = database_cursor.fetchall()
-
-    for item in data_base_rows:
-        print(item)
-
-        database_cursor.close()
-        data_base.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
