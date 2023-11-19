738568e35a04647cd4e662d5694a68a3d455e69b
Celse13 <rugiracelse13@gmail.com>
2023-11-19 08:58:24 -0800

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""Retrieving the states from the database using ORM."""
-
+"""
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
+"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    data_base = MySQLdb.connect(
-        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], port=3306
-    )
-    database_cursor = data_base.cursor()
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    query = 'SELECT cities.id, cities.name FROM cities\
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s'
+                WHERE states.name = %s", [argv[4]])
 
-    args = (sys.argv[4])
+    rows = cur.fetchall()
+    j = []
+    for i in rows:
+        j.append(i[1])
+    print(", ".join(j))
 
-    database_cursor.execute(query, args)
-
-    data_base_rows = database_cursor.fetchall()
-
-    temp = []
-    for item in data_base_rows:
-        temp.append(item[1])
-    print(", ".join(temp))
-
-    database_cursor.close()
-    data_base.close()
+    # Clean up process
+    cur.close()
+    db.close()
