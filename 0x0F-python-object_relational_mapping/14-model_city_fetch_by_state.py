43cbd6ea05233fc54d16026bb2b1d6bf40e293f6
Celse13 <rugiracelse13@gmail.com>
2023-11-19 13:53:55 -0800

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-Script that prints all City objects from the database
-"""
-from model_city import City
+"""Retrieving the states from the database using ORM."""
+
+
 from model_state import Base, State
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
-    Base.metadata.create_all(engine)
 
-    city = session.query(State, City).join(City).order_by(City.id)
-    for state, city in city:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    # Close session
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=db_engine)
+    session = Session()
+    Base.metadata.create_all(db_engine)
+
+    state_query = session.query(State, City).join(City).order_by(City.id)
+
+    for column_state, city in state_query:
+        print("{}: ({}) {}".format(column_state.name, city.id, city.name))
+
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Representation for the model state"""
+"""model state
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Represent Model"""
+    """ class class """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
