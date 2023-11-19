d505c2a936d16ade4ce7fea35e563a9babe03518
Celse13 <rugiracelse13@gmail.com>
2023-11-19 13:29:23 -0800

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
+"""Retrieving the states from the database using ORM."""
+
+
 from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, select, insert
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
 
-    Session = sessionmaker(bind=engine)
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=db_engine)
     session = Session()
-    deleteStates = session.query(State).filter(State.name.like(f'%a%')).all()
-    if deleteStates:
-        for state in deleteStates:
-            session.delete(state)
+    Base.metadata.create_all(db_engine)
+    status_query = session.query(State).filter(State.name.like(f'%a%')).all()
+
+    if status_query:
+        for item in status_query:
+            session.delete(item)
         session.commit()
 
     session.close()
