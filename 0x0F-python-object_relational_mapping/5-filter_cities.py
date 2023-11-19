2c15a74bdad895d35ded86120a0e7fd8adfb50b5
Celse13 <rugiracelse13@gmail.com>
2023-11-19 09:07:29 -0800

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """Retrieving the states from the database using ORM."""
 
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+
     data_base = MySQLdb.connect(
         host="localhost", user=sys.argv[1], passwd=sys.argv[2],
         db=sys.argv[3], port=3306
     )
     database_cursor = data_base.cursor()
 
-    query = 'SELECT cities.id, cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s'
+    query = """SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s"""
 
     args = (sys.argv[4])
 
@@ -21,10 +24,10 @@ if __name__ == "__main__":
 
     data_base_rows = database_cursor.fetchall()
 
-    temp = []
+    temp = ()
     for item in data_base_rows:
-        temp.append(item[1])
-    print(", ".join(temp))
+        temp += item
+    print(*item, sep=", ")
 
     database_cursor.close()
     data_base.close()
