d372710adc35c6dac26430a156df3547b3c91199
Celse13 <rugiracelse13@gmail.com>
2023-11-19 13:42:10 -0800

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Retrieving the states from the database using ORM."""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=db_engine)
+    session = Session()
+    Base.metadata.create_all(db_engine)
+
+    state_query = session.query(State, City).join(City).order_by(City.id)
+
+    for column_state, city in state_query:
+        print("{}: ({}) {}".format(column_state.name, city.id, city.name))
+
+    session.close()
