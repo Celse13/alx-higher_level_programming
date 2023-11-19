4fbee79f25676428a29df9f239f1e4e24adf4371
Celse13 <rugiracelse13@gmail.com>
2023-11-19 13:59:25 -0800

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""model state
-"""
-from sqlalchemy import Column, Integer, String
+"""Representation of the cities column."""
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
-Base = declarative_base()
 
-
-class State(Base):
-    """ class class """
-    __tablename__ = 'states'
+class City(Base):
+    """Representation of the cities"""
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
