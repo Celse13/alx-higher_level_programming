a0746c85cb552dc366e60f62af4f9cf9cc697d46
Celse13 <rugiracelse13@gmail.com>
2023-11-19 06:29:31 -0800

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Retrieving the states from the database using ORM."""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    data_base = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3], port=3306
+    )
+
+    database_cursor = data_base.cursor()
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE BINARY '{}'").format(sys.argv[4])
+
+    database_cursor.execute(query)
+
+    data_base_rows = database_cursor.fetchall()
+
+    for item in data_base_rows:
+        print(item)
+
+    database_cursor.close()
+    data_base.close()
