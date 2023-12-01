199ddf09437360f99cd0e409e418d5bc0271b6ae
Celse13 <rugiracelse13@gmail.com>
2023-12-01 14:32:22 -0800

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Script"""
-from requests import get
-from sys import argv
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
 
 if __name__ == "__main__":
-    address = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    com = get(address).json()
+    from requests import get
+    from sys import argv
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    commits = get(url).json()
     try:
-        for i in range(1, 10, 1):
+        for i in range(10):
             print("{}: {}".format(
-                com[i].get("sha"),
-                com[i].get("commit").get("author").get("name")))
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
